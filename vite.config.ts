23bebb85ac87ae2658deea98cd5c31efa658f566
nicolas riera <nicolariera@gmail.com>
2024-05-16 23:13:12 +0200

    updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    minify: 'terser', // or 'esbuild'
+    sourcemap: true
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
     }
   }
 })
