2bab5f51dc09227bfe69717f3ba38f52c1320642
nicolas riera <nicolariera@gmail.com>
2024-05-26 14:24:59 +0200

    updated name route for netlify

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/recomendations',
+      path: '/',
       name: 'recomendation',
       component: () => import('../views/RecomendationListView.vue')
     }
