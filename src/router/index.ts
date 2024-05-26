0f85da6187060499636329dd992491cdfa9b337c
nicolas riera <nicolariera@gmail.com>
2024-05-26 14:21:54 +0200

    update route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
       path: '/recomendations',
       name: 'recomendation',
       component: () => import('../views/RecomendationListView.vue')
