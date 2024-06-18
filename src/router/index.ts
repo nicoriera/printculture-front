1b72868314caf601d334203ff5df5b2c1e64d331
nicolas riera <nicolariera@gmail.com>
2024-06-18 23:06:47 +0200

    added screen login, logout and update screen home

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Exemple de fonction pour vérifier l'état de connexion
+function isAuthenticated() {
+  return !!localStorage.getItem('authToken') // Vous pouvez adapter ceci à votre logique d'authentification
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/recomendations',
+    name: 'recomendations',
+    component: () => import('../views/RecomendationListView.vue'),
+    meta: { requiresAuth: true } // Ajout de meta pour indiquer que cette route nécessite une authentification
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/RegisterView.vue')
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'recomendation',
-      component: () => import('../views/RecomendationListView.vue')
+  routes
+})
+
+// Garde de navigation globale
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Cette route nécessite une authentification, vérifiez l'état de connexion
+    if (!isAuthenticated()) {
+      // Utilisateur non connecté, redirigez-le vers la page de connexion
+      next({ name: 'login' })
+    } else {
+      // Utilisateur connecté, permettez l'accès
+      next()
     }
-  ]
+  } else {
+    // Route ne nécessitant pas d'authentification, permettez l'accès
+    next()
+  }
 })
 
 export default router
--- src/views/RecomendationListView.vue
+++ src/views/RecomendationListView.vue
@@ -3,6 +3,12 @@ import useHomeService, { type IRecommendation } from '@/services/recomendation'
 import { onMounted, ref, watchEffect } from 'vue'
 
 const recomendations = ref([])
+const title = ref('')
+const description = ref('')
+const tag = ref('')
+const category = ref('')
+const link = ref('')
+const isModalOpen = ref(false)
 
 const getCardColorByCategory = (category: string) => {
   switch (category) {
@@ -18,12 +24,6 @@ const getCardColorByCategory = (category: string) => {
       return 'bg-gray-200 bg-opacity-40'
   }
 }
-
-const title = ref('')
-const description = ref('')
-const tag = ref('')
-const category = ref('')
-const link = ref('')
 
 const resetForm = () => {
   title.value = ''
@@ -46,6 +46,26 @@ const fetchAddRecomendation = async () => {
     const data = await homeService.addRecommendation(recomendation)
     recomendations.value.push(data as never)
     resetForm()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const fetchDeleteRecomendation = async (movie: IRecommendation) => {
+  try {
+    const homeService = useHomeService()
+    await homeService.deleteRecommendation(movie.id!)
+    recomendations.value = recomendations.value.filter((m: IRecommendation) => m.id !== movie.id)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const fetchRecomendations = async () => {
+  try {
+    const homeService = useHomeService()
+    const data = await homeService.getRecommendations()
+    recomendations.value = data
   } catch (error) {
     console.error(error)
   }
@@ -104,31 +124,9 @@ const handleAddRecomendation = (event: Event) => {
   closeModal()
 }
 
-const fetchDeleteRecomendation = async (movie: IRecommendation) => {
-  try {
-    const homeService = useHomeService()
-    await homeService.deleteRecommendation(movie.id!)
-    recomendations.value = recomendations.value.filter((m: IRecommendation) => m.id !== movie.id)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 watchEffect(() => {
   console.log('recomendations', recomendations.value)
 })
-
-let isModalOpen = ref(false)
-
-const fetchRecomendations = async () => {
-  try {
-    const homeService = useHomeService()
-    const data = await homeService.getRecommendations()
-    recomendations.value = data
-  } catch (error) {
-    console.error(error)
-  }
-}
 
 const openModal = () => {
   const modal = document.getElementById('modal-add-recomendation')
@@ -150,79 +148,8 @@ onMounted(fetchRecomendations)
 </script>
 
 <template>
-  <div class="w-wdh">
-    <section id="navbar">
-      <header class="w-full z-20">
-        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" class="flex items-center">
-              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-                >PRINTCULTURE</span
-              >
-            </a>
-            <div class="flex items-center lg:order-2">
-              <a
-                href="#"
-                class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                >Log in</a
-              >
-
-              <button
-                data-collapse-toggle="mobile-menu-2"
-                type="button"
-                class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="mobile-menu-2"
-                aria-expanded="false"
-              >
-                <span class="sr-only">Open main menu</span>
-                <svg
-                  class="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <svg
-                  class="hidden w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-            <div
-              class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-              id="mobile-menu-2"
-            >
-              <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                <li>
-                  <a
-                    href="/"
-                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                    >Recomendation</a
-                  >
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-    </section>
-
-    <section
-      class="flex flex-col w-dwh h-full md:flex-wrap justify-between gap-2 mx-2 mt-4 md:m-10"
-    >
+  <div class="w-wdh pt-14">
+    <section class="flex w-dwh h-dvh md:h-full flex-col md:flex-wrap justify-between gap-2 mx-2">
       <div
         v-for="movie in recomendations"
         :key="(movie as IRecommendation)?.title"
@@ -265,7 +192,7 @@ onMounted(fetchRecomendations)
       <div
         v-if="isModalOpen"
         id="modal-add-recomendation"
-        class="md:w-2/4 flex p-4 md:m-4 md:shadow-lg md:border-2 md:rounded-2xl md:border-black z-900 fixed top-0 left-0 bg-white h-dvh w-dvw z-50"
+        class="md:w-2/4 flex p-4 md:m-4 md:shadow-lg md:border-2 md:rounded-2xl md:border-black z-900 fixed top-10 left-0 bg-white h-dvh w-dvw z-50"
         ref="modal"
       >
         <form class="flex-col w-full" action="submit">
